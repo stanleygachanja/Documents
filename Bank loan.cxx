24c49e8d4a557a39662fc0ab3489b6bf05f54a1a
root <root@localhost>
2024-10-03 16:30:40 +0300

    auto publish 1727962239727

---
+++ Bank loan.cxx
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    
+    int age;
+    float income;
+    
+ //prompt for user input
+ printf("bank loan eligibility");
+	printf("enter age: ");
+	scanf("%d" ,&age);
+	printf("enter income:");
+	scanf("%f" ,&income);
+	//check eligibility
+	
+	printf("check eligibility");
+	scanf("you qualify");
+	scanf("you dont qualify");
+}
